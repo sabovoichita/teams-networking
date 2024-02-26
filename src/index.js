4ef9cb34651b3c3e5395d28874b0b14cc51a7e24
Voichita Maria <sabo_voichita@yahoo.com>
2024-02-26 21:43:57 +0200

    createTeamRequest

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import "./style.css";
 
 // console.warn("app ready");
 
+function createTeamRequest() {
+  fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      promotion: "WON3",
+      members: "Your Name",
+      name: "CV",
+      url: "https://github.com/nmatei/teams-networking"
+    })
+  });
+}
+
 function getTeamAsHTML(team) {
   return `
 <tr>
@@ -28,4 +43,16 @@ function loadTeams() {
     });
   // console.warn("loadTeams", promise);
 }
+function onSubmit(e) {
+  e.preventDefault();
+  console.warn("please save all values");
+  createTeamRequest();
+  window.location.reload();
+}
+
+function initEvents() {
+  document.querySelector("#teamsForm").addEventListener("submit", onSubmit);
+}
+
+initEvents();
 loadTeams();
