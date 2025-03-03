cc97a73735e51909ccec9a708a146df793b5ebeb
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-03 12:58:38 +0100

    wait for requst to be done before we reload

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ function $(selector) {
 }
 
 function createTeamRequest(team) {
-  fetch("http://localhost:3000/teams-json/create", {
+  return fetch("http://localhost:3000/teams-json/create", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(team)
-  });
+  }).then(r => r.json());
 }
 
 function getTeamAsHTML(team) {
@@ -57,8 +57,12 @@ function onSubmit(e) {
   };
 
   // console.warn(team);
-  createTeamRequest(team);
-  window.location.reload();
+  createTeamRequest(team).then(status => {
+    // console.warn("ready status", status);
+    if (status.success) {
+      window.location.reload();
+    }
+  });
 }
 
 function initEvents() {
