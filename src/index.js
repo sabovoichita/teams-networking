7f519a90999fed0f61e0d9562a108e8b09d7e059
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-12 18:38:42 +0100

    areTeamsEquals function(66)

--- src/index.js
+++ src/index.js
@@ -57,9 +57,33 @@ function getTeamAsHTML(team) {
   `;
 }
 
+function areTeamsEquals(renderedTeams, teams) {
+  if (renderedTeams === teams) {
+    // console.info("same array");
+    return true;
+  }
+  if (renderedTeams.length === teams.length) {
+    const eq = renderedTeams.every((team, i) => team === teams[i]);
+    if (eq) {
+      // console.warn("same content in diferent arrays");
+      return true;
+    }
+    return false;
+  }
+}
+let renderedTeams = [];
 function renderTeams(teams) {
+  // console.time("eq-check");
+  if (areTeamsEquals(renderedTeams, teams)) {
+    // console.timeEnd("eq-check");
+    return;
+  }
+  // console.timeEnd("eq-check");
+  renderedTeams = teams;
+  console.time("render");
   const teamsHTML = teams.map(getTeamAsHTML);
   $("#teamsTable tbody").innerHTML = teamsHTML.join("");
+  console.timeEnd("render");
 }
 
 function loadTeams() {
